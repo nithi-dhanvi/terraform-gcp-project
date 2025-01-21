b774890988b5c44b0fef3578dd1c1a65a508650e
nithi-dhanvi <116012455+nithi-dhanvi@users.noreply.github.com>
2025-01-21 09:00:35 -0600

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,12 @@
+resource "google_compute_network" "default" {
+  name = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnets" {
+  count         = length(var.subnets)
+  name          = var.subnets[count.index].name
+  ip_cidr_range = var.subnets[count.index].ip_cidr_range
+  region        = var.subnets[count.index].region
+  network       = google_compute_network.default.id
+}
