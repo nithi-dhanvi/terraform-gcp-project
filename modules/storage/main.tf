020f03d5e09eac655053f912fcc5aa81a424ad85
nithi-dhanvi <116012455+nithi-dhanvi@users.noreply.github.com>
2025-01-21 09:32:12 -0600

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -8,14 +8,15 @@ resource "google_compute_disk" "additional_storage" {
   #type  = "pd-standard"        # Options: pd-standard, pd-ssd, pd-balanced
 
   labels = {
-    environment = "test"       # Add custom labels
+    #environment = "test"       # Add custom labels
+    environment = var.env
   }
 }
 
 # Attach Persistent Disk to an Existing Instance
-resource "google_compute_attached_disk" "attach_additional_storage" {
-  instance = "instance-name"                 # Replace with the name of your instance
-  zone     = google_compute_disk.additional_storage.zone
-  disk     = google_compute_disk.additional_storage.name
-  device_name = "additional-disk"
+#resource "google_compute_attached_disk" "attach_additional_storage" {
+#  instance = "instance-name"                 # Replace with the name of your instance
+#  zone     = google_compute_disk.additional_storage.zone
+#  disk     = google_compute_disk.additional_storage.name
+#  device_name = "additional-disk"
 }
