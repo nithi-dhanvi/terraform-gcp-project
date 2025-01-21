670b2d5f50cc730db731c617a3c9ff76dc575b83
nithi-dhanvi <116012455+nithi-dhanvi@users.noreply.github.com>
2025-01-21 09:33:52 -0600

    Update variables.tf

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,1 +1,10 @@
+variable "disk name" {
+  description = "The name of the compute instance"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type of the instance"
+  type        = string
+}
 
